ec1f9e9da5d35b317b3ed62fbecc0c42b4d237bf
Nat Peterson <npeterson@gmail.com>
2024-11-30 12:44:08 -0800

    setup script

---
+++ README.md
@@ -0,0 +1,9 @@
+Templates, utilities, etc. from [Dazbo](https://aoc.just2good.co.uk/)  Thanks, dude!
+
+and then [python config for vscode](https://stackoverflow.com/questions/53653083/how-to-correctly-set-pythonpath-for-visual-studio-code)
+
+`.env` like:
+```sh
+PYTHONPATH=src
+AOC_SESSION_COOKIE=[from your browser...]
+```
---
+++ day.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# usage:  ./day.sh 1
+# creates day01 directory and copies template files to it
+zfill() {
+  local n=$1
+  local w=2
+  printf "%0${w}d" $n
+}
+
+day=$1
+# exit if day is not a number
+if ! [[ $day =~ ^[0-9]+$ ]] || [ $day -lt 1 ] || [ $day -gt 25 ]; then
+    echo "Usage: $0 <day>"
+    echo "<day> must be a number between 1 and 25"
+    exit 1
+fi
+
+day0=$(zfill $1)
+date=$(date "+%Y-%m-%d")
+
+mkdir -p src/day$day0
+cp src/day00/* src/day$day0
+mv src/day$day0/day00.py src/day$day0/day$day0.py
+# replace DAY = 1 with DAY = $day
+sed -i "s/DAY = 1/DAY = $day/g" src/day$day0/day$day0.py
+# replace "https://adventofcode.com/2024/day/1" with $day
+sed -i "s|https://adventofcode.com/2024/day/1|https://adventofcode.com/2024/day/$day|g" src/day$day0/day$day0.py
+# populate Date like "Date: 2023-12-01"
+sed -i "s/Date: 2023-12-01/Date: $date/g" src/day$day0/day$day0.py
+
+echo "Day $day0 ready"
