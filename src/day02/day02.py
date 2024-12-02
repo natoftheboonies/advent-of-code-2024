7e06c163db634c7854a8b0e6f0066355756cc45e
Nat Peterson <npeterson@gmail.com>
2024-12-02 07:33:31 -0800

    day02 tidy

--- src/day02/day02.py
+++ src/day02/day02.py
@@ -3,11 +3,6 @@ Author: Nat with Darren's Template
 Date: 2024-12-01
 
 Solving https://adventofcode.com/2024/day/2
-
-Part 1:
-
-Part 2:
-
 """
 
 import logging
@@ -28,42 +23,12 @@ except ValueError as e:
 
 
 def test_safe(nums):
-    decrease = None
-
-    ok = False
-    for i, n in enumerate(nums):
-        if i == 0:
-            continue
-        if decrease == True and n >= nums[i - 1]:
-            break
-        if decrease == False and n <= nums[i - 1]:
-            break
-        if decrease == None and n == nums[i - 1]:
-            logger.debug("same failed part 1: %s", nums)
-            break
-        if decrease == None and n < nums[i - 1]:
-            decrease = True
-            logger.debug("detected decrease: %s", nums)
-        if decrease == None and n > nums[i - 1]:
-            decrease = False
-            logger.debug("detected increase: %s", nums)
-    else:
-        ok = True
-        logger.debug("passed part 1: %s", nums)
-    if not ok:
-        return False
-
-    for i, n in enumerate(nums):
-        if i == 0:
-            continue
-        if abs(n - nums[i - 1]) > 3:
-            logger.debug("failed part 2: %s", n)
-            break
-
-    else:
-        logger.debug("passed part 2: %s", nums)
-        return True
-    return False
+    return (  # always increasing or always decreasing
+        all(nums[i] > nums[i + 1] for i in range(len(nums) - 1))
+        or all(nums[i] < nums[i + 1] for i in range(len(nums) - 1))
+    ) and not any(  # no difference greater than 3
+        abs(nums[i + 1] - nums[i]) > 3 for i in range(len(nums) - 1)
+    )
 
 
 def main():
@@ -77,19 +42,15 @@ def main():
     unsafe = []
     # Part 1
     logger.info("Part 1")
-    result = 0
+    safe = 0
     for line in data:
         nums = [int(x) for x in line.split()]
         if test_safe(nums):
-            result += 1
+            safe += 1
         else:
             unsafe.append(nums)
 
-        # if len(nums) != len(set(nums)):
-        #     continue
-
-    # not 298, 485, 408
-    logger.info("#1: %s", result)
+    logger.info("#1: %s", safe)
 
     # part 2
     newly_safe = 0
@@ -100,7 +61,7 @@ def main():
                 newly_safe += 1
                 break
 
-    logger.info("#2: %s", result + newly_safe)
+    logger.info("#2: %s", safe + newly_safe)
 
 
 if __name__ == "__main__":
