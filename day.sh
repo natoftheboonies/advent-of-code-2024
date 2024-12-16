a96900a428b1d0417c0e28f83c2beb77427dccca
Nat Peterson <natpeterson@gmail.com>
2024-12-15 16:46:39 -0800

    python > sed battles

--- day.sh
+++ day.sh
@@ -1,31 +1,32 @@
-#!/bin/bash
-# usage:  ./day.sh 1
-# creates day01 directory and copies template files to it
-zfill() {
-  local n=$1
-  local w=2
-  printf "%0${w}d" $n
-}
+#!/usr/bin/env python3
+import os
+import sys
+from datetime import datetime
 
-day=$1
-# exit if day is not a number
-if ! [[ $day =~ ^[0-9]+$ ]] || [ $day -lt 1 ] || [ $day -gt 25 ]; then
-    echo "Usage: $0 <day>"
-    echo "<day> must be a number between 1 and 25"
-    exit 1
-fi
+if len(sys.argv) != 2:
+  print("Usage: {} <day>".format(sys.argv[0]))
+  sys.exit(1)
 
-day0=$(zfill $1)
-date=$(date "+%Y-%m-%d")
+day = sys.argv[1]
 
-mkdir -p src/day$day0
-cp src/day00/* src/day$day0
-mv src/day$day0/day00.py src/day$day0/day$day0.py
-# replace DAY = 1 with DAY = $day
-sed -i'' -E "s/DAY = 1/DAY = $day/g" src/day$day0/day$day0.py
-# replace "https://adventofcode.com/2024/day/1" with $day
-sed -i'' -E "s|https://adventofcode.com/2024/day/1|https://adventofcode.com/2024/day/$day|g" src/day$day0/day$day0.py
-# populate Date like "Date: 2023-12-01"
-sed -i'' -E "s/Date: 2023-12-01/Date: $date/g" src/day$day0/day$day0.py
+if not day.isdigit() or not (1 <= int(day) <= 25):
+  print("Usage: {} <day>".format(sys.argv[0]))
+  print("<day> must be a number between 1 and 25")
+  sys.exit(1)
 
-echo "Day $day0 ready"
+day0 = day.zfill(2)
+date = datetime.now().strftime("%Y-%m-%d")
+
+os.makedirs(f"src/day{day0}", exist_ok=True)
+
+for filename in os.listdir("src/day00"):
+  src = os.path.join("src/day00", filename)
+  dst = os.path.join(f"src/day{day0}", filename.replace("day00", f"day{day0}"))
+  with open(src) as fsrc, open(dst, 'w') as fdst:
+    content = fsrc.read()
+    content = content.replace("DAY = 1", f"DAY = {day}")
+    content = content.replace("https://adventofcode.com/2024/day/1", f"https://adventofcode.com/2024/day/{day}")
+    content = content.replace("Date: 2023-12-01", f"Date: {date}")
+    fdst.write(content)
+
+print(f"Day {day0} ready")
