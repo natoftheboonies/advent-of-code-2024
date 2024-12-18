1d3bb9e16ed31b332e88a8263d4f74ad22ab2c19
Nat Peterson <natpeterson@gmail.com>
2024-12-17 19:05:41 -0800

    d17 tidy

--- src/day17/day17.py
+++ src/day17/day17.py
@@ -41,15 +41,13 @@ def run_program(reg, program):
     while ip < len(program):
         opcode = program[ip]
         operand = program[ip + 1]
-        # logger.debug("ip=%d, op=%s, reg=%s", ip, op, reg)
         if opcode == 0:  # adv
-            reg[0] = reg[0] // (2 ** combo(operand))
+            reg[0] = reg[0] >> combo(operand)
         elif opcode == 1:  # bxl
             reg[1] = reg[1] ^ operand
         elif opcode == 2:  # bst
             reg[1] = combo(operand) % 8
         elif opcode == 3:  # jnz
-            assert operand % 2 == 0
             if reg[0] != 0:
                 ip = operand
                 continue
@@ -58,12 +56,10 @@ def run_program(reg, program):
         elif opcode == 5:  # out
             output.append(combo(operand) % 8)
         elif opcode == 6:  # bdv
-            reg[1] = reg[0] // (2 ** combo(operand))
+            reg[1] = reg[0] >> combo(operand)
         elif opcode == 7:  # cdv
-            reg[2] = reg[0] // (2 ** combo(operand))
+            reg[2] = reg[0] >> combo(operand)
         ip += 2
-    # logger.debug("output=%s", output)
-    # not 7,7,7,7,7,7,7,7,7
     return output
 
 
@@ -72,14 +68,13 @@ def main():
     # puzzle = locations.sample_input_file
     with open(puzzle, mode="rt") as f:
         data = f.read()
-        registers, program = data.split("\n\n")
-        registers = registers.splitlines()
-        program = list(map(int, program.strip().split(": ")[1].split(",")))
-        assert len(program) % 2 == 0
-        reg = [int(line.split(": ")[1]) for line in registers]
-        logger.debug("reg=%s, prog=%s", reg, program)
-        output = run_program(reg, program)
-        logger.info("Part 1: %s", ",".join(map(str, output)))
+    registers, program = data.split("\n\n")
+    registers = registers.splitlines()
+    program = list(map(int, program.strip().split(": ")[1].split(",")))
+    assert len(program) % 2 == 0
+    reg = [int(line.split(": ")[1]) for line in registers]
+    output = run_program(reg, program)
+    logger.info("Part 1: %s", ",".join(map(str, output)))
 
     # time to find a teacher :)
     # part 2 credit: https://www.reddit.com/r/adventofcode/comments/1hg38ah/comment/m2gfogr/
@@ -89,19 +84,12 @@ def main():
         while True:
             reg = [test_a, 0, 0]
             output = run_program(reg, program)
-            # logger.debug(
-            #     "test_a=%d, output=%s, sub_program=%s", test_a, output, sub_program
-            # )
-            if output == list(sub_program):
+            if output == sub_program:
                 return test_a
             test_a += 1
 
-    # reg_a = find_reg_a(program[-3:])
-    # logger.debug(run_program([reg_a, 0, 0], program))
     reg_a = find_reg_a(program)
-
     logger.info("Part 2: %s", reg_a)
-    # logger.debug(data)
 
 
 if __name__ == "__main__":
